f76159fdeea522a394db2b8303fbe2399d70a42a
Christopher-OOC <olojedechristopher24@gmail.com>
2024-07-18 09:02:53 +0100

    commit

--- src/main/java/com/example/email_sender/service/SendEmailService.java
+++ src/main/java/com/example/email_sender/service/SendEmailService.java
@@ -1,6 +1,11 @@
 package com.example.email_sender.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.security.GeneralSecurityException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import org.eclipse.angus.mail.util.MailSSLSocketFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -9,13 +14,31 @@ import org.springframework.stereotype.Service;
 @Service
 public class SendEmailService {
 	
-	@Autowired
 	private JavaMailSender javaMailSender;
 	
 	@Value("${spring.mail.username}")
 	private String fromEmailId;
 	
+	public SendEmailService(JavaMailSender javaMailSender) throws GeneralSecurityException {
+		this.javaMailSender = javaMailSender;
+		
+		MailSSLSocketFactory sf = new MailSSLSocketFactory();
+		sf.setTrustAllHosts(true);
+		
+		Properties props = new Properties();
+		
+		props.put("mail.imap.ssl.trust", "*");
+		props.put("mail.imap.ssl.socketFactory", sf);
+	
+	
+		System.setProperties(props);
+		System.out.println("HELLO!!!");
+		
+	}
+	
 	public void sendEamil(String recipient, String body, String subject) {
+		
+		
 		SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
 		simpleMailMessage.setFrom(fromEmailId);
 		simpleMailMessage.setTo(recipient);
